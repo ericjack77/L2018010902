c616df6a1aa6c292b0a1d77ab3cf501ea62596fc
ericchiu <ericjack7@gmail.com>
2018-01-09 16:41:49 +0800

    做多版本選擇

--- app/src/main/java/com/example/student/l2018010902/MainActivity.java
+++ app/src/main/java/com/example/student/l2018010902/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.student.l2018010902;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,23 +18,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+                nm=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                if (Build.VERSION.SDK_INT>=26)
+                {
+                    regchannel1();
+                }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    public  void regchannel1()
+    {
         channelLove = new NotificationChannel(
                 idLove,
                 "Channel Love",
                 NotificationManager.IMPORTANCE_HIGH);
 
-                channelLove.setDescription("重要");
-                channelLove.enableLights(true);
-                channelLove.enableVibration(true);
-
-                nm=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-                nm.createNotificationChannel(channelLove);
-
+        channelLove.setDescription("重要");
+        channelLove.enableLights(true);
+        channelLove.enableVibration(true);
+        nm.createNotificationChannel(channelLove);
     }
 
+    @TargetApi(Build.VERSION_CODES.O)
+    @SuppressWarnings("deprecation")
     public void click1(View v)
     {
-        Notification.Builder builder =new Notification.Builder(MainActivity.this,idLove);
+        Notification.Builder builder;
+        if(Build.VERSION.SDK_INT>=26)
+        {
+            builder =new Notification.Builder(MainActivity.this,idLove);
+        }
+        else
+        {
+            builder =new Notification.Builder(MainActivity.this);
+        }
         builder.setContentTitle("測試");
         builder.setContentText("Content");
         builder.setSmallIcon(R.drawable.ic_launcher_foreground);
